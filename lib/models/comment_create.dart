825b8f5bb1f45bdc8664ba528336956d4e177e10
Nguyen Hoa Ninh Dan <dh52007049@student.stu.edu.vn>
2024-05-17 19:12:10 +0700

    Comment - Error display out srceen comment

---
+++ lib/models/comment_create.dart
@@ -0,0 +1,9 @@
+class CommentPost {
+  late final String postId;
+  late final String content;
+
+  CommentPost({
+    required this.postId,
+    required this.content,
+  });
+}
---
+++ lib/views/post_edit_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class PostEditScreen extends StatefulWidget {
+  const PostEditScreen({super.key});
+
+  @override
+  State<PostEditScreen> createState() => _PostEditScreenState();
+}
+
+class _PostEditScreenState extends State<PostEditScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
